8b157de0b58db70589b30f80bd5526f524a2f03e
mcmak <maimcmak@outlook.com>
2022-09-09 11:20:25 +0800

    environment completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+//Import route
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+//Import Antd
+import 'antd/dist/antd.min.css'
+import { Button } from 'antd';
+
+//Import route compontents
+import Layout from '@/pages/layout/layout'
+import Login from '@/pages/login/login'
+
 const App = () => {
   return(
-    <div>
-      Hello!
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+    
   )
 }
 
